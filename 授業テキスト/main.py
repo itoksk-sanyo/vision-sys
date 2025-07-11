6958e8b2310663b40da7e843997e760be0c9463f
keisuke <k_ito@st.sanyo.ed.jp>
2025-07-11 02:48:03 +0000

    comi

---
+++ 授業テキスト/main.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
